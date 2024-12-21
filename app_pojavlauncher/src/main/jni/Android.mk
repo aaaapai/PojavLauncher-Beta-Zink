c201fecfee14d5a0d05a7e3c04e0d14f82c19893
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-22 00:42:33 +0800

    Update Android.mk

--- app_pojavlauncher/src/main/jni/Android.mk
+++ app_pojavlauncher/src/main/jni/Android.mk
@@ -41,8 +41,8 @@ LOCAL_SRC_FILES := \
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 LOCAL_CFLAGS += -DADRENO_POSSIBLE
-LOCAL_CFLAGS += -flto=thin -fwhole-program-vtables -mllvm -polly -Weverything
-# LOCAL_LDLAGS += -flto=thin -fuse-ld=lld
+LOCAL_CFLAGS += -Ofast -flto=thin -fwhole-program-vtables -mllvm -polly -Weverything -std=c2x
+LOCAL_LDLAGS += -flto=thin
 LOCAL_LDLIBS += -lEGL -lGLESv3
 endif
 include $(BUILD_SHARED_LIBRARY)
