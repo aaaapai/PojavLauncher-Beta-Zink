f6d3e16a475d4bbe24ce9501ea073e83395b8167
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-26 23:20:21 +0800

    Update egl_loader.c

--- app_pojavlauncher/src/main/jni/ctxbridges/egl_loader.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/egl_loader.c
@@ -57,5 +57,4 @@ void dlsym_EGL(void) {
     eglTerminate_p = dlsym(dl_handle, "eglTerminate");
     eglGetCurrentSurface_p = dlsym(dl_handle,"eglGetCurrentSurface");
     eglQuerySurface_p = dlsym(dl_handle, "eglQuerySurface");
-    eglQueryString_p = dlsym(dl_handle, "eglQueryString");
 }
