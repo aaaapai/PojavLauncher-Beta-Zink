f56708f1e710956bd51421522a667a906ace8af8
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-02-25 23:10:03 +0800

    Update JavaGUILauncherActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JavaGUILauncherActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JavaGUILauncherActivity.java
@@ -222,11 +222,7 @@ public class JavaGUILauncherActivity extends BaseActivity implements View.OnTouc
         }
         Runtime selectedRuntime = MultiRTUtils.forceReread(nearestRuntime);
         int selectedJavaVersion = Math.max(javaVersion, selectedRuntime.javaVersion);
-        // Don't allow versions higher than Java 17 because our caciocavallo implementation does not allow for it
-        if (selectedJavaVersion > 17) {
-            finalErrorDialog(getString(R.string.execute_jar_incompatible_runtime, selectedJavaVersion));
-            return null;
-        }
+        // Allow versions higher than Java 17 because our caciocavallo implementation allows for it
         return selectedRuntime;
     }
 
