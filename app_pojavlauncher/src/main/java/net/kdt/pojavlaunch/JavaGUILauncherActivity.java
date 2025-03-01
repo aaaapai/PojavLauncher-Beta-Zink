50c053ee6fedbb520bf5d6060c09dc3fb397dd02
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-01 18:15:56 +0800

    Update JavaGUILauncherActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JavaGUILauncherActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JavaGUILauncherActivity.java
@@ -385,7 +385,7 @@ public class JavaGUILauncherActivity extends BaseActivity implements View.OnTouc
 
             Logger.appendToLog("Info: Java arguments: " + Arrays.toString(javaArgList.toArray(new String[0])));
 
-            JREUtils.launchWithUtils(this, runtime, null, null, javaArgList, LauncherPreferences.PREF_CUSTOM_JAVA_ARGS);
+            JREUtils.launchWithUtils(this, runtime, null, javaArgList, AllSettings.getJavaArgs().getValue(), null);
         } catch (Throwable th) {
             Tools.showError(this, th, true);
         }
