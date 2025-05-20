697cad7a7ef581683fc77b2562891610ee6f63bf
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-20 12:44:42 +0800

    Update egl_bridge.c

--- app_pojavlauncher/src/main/jni/pojav/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/pojav/egl_bridge.c
@@ -80,6 +80,10 @@ EXTERNAL_API void pojavTerminate(void) {
             potatoBridge.eglContext = EGL_NO_CONTEXT;
             potatoBridge.eglDisplay = EGL_NO_DISPLAY;
             potatoBridge.eglSurface = EGL_NO_SURFACE;
+
+            pthread_t t;
+            pthread_create(&t, NULL, egl_make_current, (void *)ctx);
+            usleep(100*1000);
         } break;
         case RENDERER_VK_ZINK:
         case RENDERER_VK_ZINK_XXX1:
