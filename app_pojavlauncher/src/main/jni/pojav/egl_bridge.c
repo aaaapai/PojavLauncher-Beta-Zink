7a285a061cf9414d6da8aa730cf0ef45f110ea73
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-01 12:26:10 +0800

    Update egl_bridge.c

--- app_pojavlauncher/src/main/jni/pojav/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/pojav/egl_bridge.c
@@ -42,6 +42,7 @@
 /* Consider GLFW_NO_API as Vulkan API */
 #define GLFW_NO_API 0
 #define GLFW_OPENGL_API 0x30001
+#define GLFW_OPENGL_ES_API 0x30002
 // region OSMESA internals
 
 // This means that the function is an external API and that it will be used
@@ -225,14 +226,6 @@ static int pojavInitOpenGL(void) {
 
     if (!strncmp("opengles", renderer, 8))
     {
-        load_vulkan();
-        setenv("GALLIUM_DRIVER", "zink", 1);
-        setenv("MESA_LOADER_DRIVER_OVERRIDE", "zink", 1);
-        setenv("MESA_LOG_LEVEL", "debug", 1);
-        setenv("MESA_LOG", "file", 1);
-        setenv("MESA_GL_VERSION_OVERRIDE", "4.6", 1);
-        setenv("MESA_GLSL_VERSION_OVERRIDE", "460", 1);
-        setenv("GALLIUM_DRIVER", "zink", 1);
         ConfigBridgeTbl();
         pojav_environ->config_renderer = RENDERER_GL4ES;
         if (pojav_environ->config_bridge == 0) set_gl_bridge_tbl();
@@ -343,7 +336,6 @@ EXTERNAL_API void pojavSetWindowHint(int hint, int value) {
             break;
         default:
             printf("GLFW: Unimplemented API 0x%x\n", value);
-            abort();
     }
 }
 
